c251784ce7b0a7242b4751c4882bd8d8188cc47c
Christian <christianisenberg@gmail.com>
2020-02-02 00:33:55 -0300

    It works git add .gitignore

--- s2dtest_rpi.c
+++ s2dtest_rpi.c
@@ -9,7 +9,9 @@
 
 
 S2D_Window *window;
-S2D_Image  *img_1280;
+S2D_Image  *gst_s2d_image;
+S2D_Image  *button;
+
 S2D_Text   *txt_msg;
 const char *font = "media/UbuntuMono-R.ttf";
 //const char *font = "media/Roboto-Condensed.otf";
@@ -32,28 +34,40 @@ gint64 lastFrameCount = 0;
 
 float currentFps = 0;
 
+S2D_Image *S2D_CreateImageGST(int width, int heigth) {
+  
+	S2D_Image *img = (S2D_Image *) malloc(sizeof(S2D_Image));
+	// here is the secret
+	img->surface = SDL_CreateRGBSurface(0,width,height,32, 0, 0, 0, 0);
 
-float getCpuUsage () {
-	
-	long double a[4], b[4], loadavg;
-    FILE *fp;
+	// Default values
+	img->texture_id = 0;
+	img->path = "";
+	img->x = 0;
+	img->y = 0;
+	img->color.r = 1.f;
+	img->color.g = 1.f;
+	img->color.b = 1.f;
+	img->color.a = 1.f;
+	img->orig_width  = width;
+	img->orig_height = heigth;
+	img->width  = width;
+	img->height = heigth;
+	img->rotate = 0;
+	img->rx = 0;
+	img->ry = 0;
+	img->format = GL_RGB;
 
-    fp = fopen("/proc/stat","r");
-    fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&a[0],&a[1],&a[2],&a[3]);
-    fclose(fp);
-    sleep(1);
-
-    fp = fopen("/proc/stat","r");
-    fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&b[0],&b[1],&b[2],&b[3]);
-    fclose(fp);
-
-    loadavg = ((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3]));
+	return img;
 }
+
 
 // carrega os recursos
 void load () {
 	
-	img_1280 = S2D_CreateImage("media/1280.png");
+	button = S2D_CreateImage("media/button.png");
+	gst_s2d_image = S2D_CreateImageGST(1280,720);
+
 
     txt_msg = S2D_CreateText(font, "Testando texto 1234% ! [/chdata]", 14);
 	txt_msg->x = 50;
@@ -62,11 +76,10 @@ void load () {
 	// txt_msg->color.g = 0;
 	// txt_msg->color.b = 0;
 
-    img_1280->width = 480;
-    img_1280->height = 320;
+	gst_s2d_image->x = 0;
     
 	// bind para poder setar pixels dessa textura
-       glBindTexture(GL_TEXTURE_2D, img_1280->texture_id);
+       glBindTexture(GL_TEXTURE_2D, gst_s2d_image->texture_id);
 
 }
 
@@ -84,8 +97,8 @@ int initGST2() {
 	caps = gst_caps_new_simple (
 		"video/x-raw",
 		"format", G_TYPE_STRING, "RGB",
-		"width", G_TYPE_INT, img_1280->orig_width,
-		"height", G_TYPE_INT, img_1280->orig_height,
+		"width", G_TYPE_INT, gst_s2d_image->orig_width,
+		"height", G_TYPE_INT, gst_s2d_image->orig_height,
 		"pixel-aspect-ratio", GST_TYPE_FRACTION, 1, 1,
 		NULL
 	);
@@ -116,9 +129,9 @@ int initGST2() {
 int initGST() {
 
 	pipeline = gst_parse_launch ("v4l2src device=/dev/video2 \
-	! image/jpeg,width=640,height=480,framerate=15/1 \
-	! videorate name=rate ! image/jpeg,width=640,height=480,framerate=15/1 ! jpegdec ! queue \
-   	! videoscale ! queue ! videoconvert ! video/x-raw,width=480,height=320,format=RGB,pixel-aspect-ratio=1/1 \
+	! image/jpeg,width=1280,height=720,framerate=30/1 \
+	! videorate name=rate ! image/jpeg,width=1280,height=720,framerate=30/1 ! jpegdec ! queue \
+   	! videoconvert ! video/x-raw,width=1280,height=720,format=RGB,pixel-aspect-ratio=1/1 \
     ! appsink max-buffers=5 drop=1 wait-on-eos=false emit-signals=true name=sink", NULL);
     
 	//source = gst_bin_get_by_name(GST_BIN(pipeline), "source");
@@ -138,55 +151,52 @@ int initGST() {
 
 void updateTexture() {
 
-    g_signal_emit_by_name (sink, "pull-sample", &sample, NULL);
+	g_signal_emit_by_name (sink, "pull-sample", &sample, NULL);
 
-    /* if we have a buffer now, convert it to a pixbuf */
-    if (sample) {
+	/* if we have a buffer now, convert it to a pixbuf */
+	if (sample) {
 
-        GstBuffer *buffer;
-        GstCaps *caps;
-        GstStructure *s;
+		GstBuffer *buffer;
+		GstCaps *caps;
+		GstStructure *s;
 
-        /* get the caps structure */
-        caps = gst_sample_get_caps (sample);
-        s = gst_caps_get_structure (caps, 0);
-        gst_structure_get_int (s, "width", &width);
-        gst_structure_get_int (s, "height", &height);
+		/* get the caps structure */
+		caps = gst_sample_get_caps (sample);
+		s = gst_caps_get_structure (caps, 0);
+		gst_structure_get_int (s, "width", &width);
+		gst_structure_get_int (s, "height", &height);
 
-        /* get the pixbuf */
-        buffer = gst_sample_get_buffer (sample);
+		/* get the pixbuf */
+		buffer = gst_sample_get_buffer (sample);
 
-        if (gst_buffer_map (buffer, &map, GST_MAP_READ)) {
+		if (gst_buffer_map (buffer, &map, GST_MAP_READ)) {
+			glBindTexture(GL_TEXTURE_2D, gst_s2d_image->texture_id);
+			glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 1280, 720, GL_RGB, GL_UNSIGNED_BYTE, map.data);
+			gst_buffer_unmap (buffer, &map);
+		} else {
+			printf ("could not map\n");
+		}
 
-	    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 480, 320, GL_RGB, GL_UNSIGNED_BYTE, map.data);
-            
-            gst_buffer_unmap (buffer, &map);
+		gst_sample_unref (sample);
 
-        } else {
-            printf ("could not map\n");
-        }
-
-        gst_sample_unref (sample);
-
-    } else {
-        printf ("could not make snapshot\n");
-    }
+	} else {
+		printf ("could not make snapshot\n");
+	}
 }
 
 // função de desenho, é chamada na mesma frequência que o fps (ex 30fps)
 void render () {
-	updateTexture();
+     S2D_DrawImage(gst_s2d_image);
+     S2D_DrawImage(button);
 	
-	// S2D_DrawQuad(0, 0, 0, 1, 1, 1,
-       //      480, 0, 0, 1, 1, 1,
-          //   480, 320, 0, 1, 1, 1,
-            // 0, 320, 0, 1, 1, 1);
-     S2D_DrawImage(img_1280);
-	// S2D_DrawText(txt_msg);
-	
-//	S2D_DrawImage(screen);
+	S2D_DrawQuad(0, 0, 0, 1, 1, 0.5f,
+            480, 0, 0, 1, 1, 0.5f,
+            480, 320, 0, 1, 1, 0.5f,
+            0, 320, 0, 1, 1, 0.5f);
+	S2D_DrawText(txt_msg);
 
-	// printf("\nFPS: %f", calcFps());
+	// call once to pull gst frame
+	updateTexture();
 }
 
 // função de lógica, é chamada mais vezes que a render
@@ -206,9 +216,9 @@ int main ( int argc, char **argv ) {
 
 	S2D_Diagnostics(true);
 	// carrega Simple2D
-	window = S2D_CreateWindow("AAAA", 480, 320, update, render, S2D_RESIZABLE);
+	window = S2D_CreateWindow("AAAA", 1280, 720, update, render, SDL_WINDOW_BORDERLESS);
 	window->viewport.mode = S2D_FIXED;
-    window->fps_cap = 30;
+    window->fps_cap = 60;
 	window->vsync = false;
 	S2D_ShowCursor(true);
 	

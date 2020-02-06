d96cec45942fef80021e5b1918f6b7da89358be3
Christian Isenberg <christianisenberg@gmail.com>
2020-02-06 00:10:34 +0000

    Faster pipeline on arm

--- s2dtest_rpi.c
+++ s2dtest_rpi.c
@@ -4,7 +4,7 @@
 #include "src/video_cap.hpp"
 #include "src/tests.hpp"
 
-// gcc s2dtest_rpi.c -lsimple2d -I/usr/include/SDL2 -I/opt/vc/include -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -D_REENTRANT -L/usr/lib -Wl,-rpath=/opt/vc/lib -Wl,--enable-new-dtags -lSDL2 -lm -I/opt/vc/include/ -L/opt/vc/lib -lGLESv2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf `pkg-config --cflags --libs gstreamer-1.0` -o s2dtest
+// g++ s2dtest_rpi.c src/video_cap.cpp -I/usr/include/SDL2 -D_REENTRANT -Wl,-rpath=/opt/vc/lib -lGLESv2 -lsimple2d -L/usr/lib -pthread -lSDL2 -lGL -lm -lSDL2_image -lSDL2_mixer -lSDL2_ttf `pkg-config --cflags --libs gstreamer-1.0` -o s2dtest
 
 // gcc s2dtest_rpi.c `simple2d --libs` `pkg-config --cflags --libs gstreamer-1.0` -o s2dtest
 
@@ -30,18 +30,19 @@ void load () {
 	gst_s2d_image = S2D_CreateEmptyImage(1280,720);
 
 	txt_msg = S2D_CreateText(font, "Testando texto 1234! [/chdata]", 20);
-	S2D_DrawTextShadow(txt_msg);
+	// S2D_DrawTextShadow(txt_msg);
 	//  S2D_SetText(txt_msg, "abacate");
 
 	
 	gauge_text = S2D_CreateText(font, "11", 24);
+	
 	txt_msg->x = 50;
 	txt_msg->y = 50;
 	txt_msg->color.r = 0;
 	txt_msg->color.g = 1;
 	txt_msg->color.b = 1;
 
-	gst_s2d_image->width = 1280;
+	// gst_s2d_image->width = 1280;
 }
 
 
@@ -85,12 +86,11 @@ int main ( int argc, char **argv ) {
 	video_cap = new VideoCap(1280,720);
 
 	S2D_Diagnostics(true);
-	// carrega Simple2D
-	window = S2D_CreateWindow("AAAA", 1366, 720, update, render, SDL_WINDOW_BORDERLESS);
-	window->viewport.mode = S2D_FIXED;
+    window = S2D_CreateWindow("", 1366, 768, update, render, S2D_FULLSCREEN);
+    window->viewport.mode = S2D_FIXED;
     window->fps_cap = 30;
 	window->vsync = false;
-	// S2D_ShowCursor(true);
+	S2D_ShowCursor(false);
 	
 	// inicia interface e bloqueia
 	S2D_Show(window);
--- src/video_cap.cpp
+++ src/video_cap.cpp
@@ -13,11 +13,10 @@ VideoCap::VideoCap(int width, int height)
 	this->width = width;
 	this->height = height;
 
-	GstElement *pipeline = gst_parse_launch ("v4l2src device=/dev/video2 \
-	! image/jpeg,width=1280,height=720,framerate=30/1 \
-	! videorate name=rate ! image/jpeg,width=1280,height=720,framerate=30/1 ! jpegdec ! queue \
+	GstElement *pipeline = gst_parse_launch ("v4l2src device=/dev/video0 \
+	! image/jpeg,width=1280,height=720,framerate=30/1 ! jpegdec \
 	! videoconvert ! video/x-raw,width=1280,height=720,format=RGB,pixel-aspect-ratio=1/1 \
-	! appsink max-buffers=1 drop=1 wait-on-eos=false emit-signals=true name=sink", NULL);
+	! appsink max-buffers=1 drop=1 wait-on-eos=false emit-signals=true name=sink sync=false", NULL);
 
 	this->sink = gst_bin_get_by_name(GST_BIN(pipeline), "sink");
 	// GstElement *rate = gst_bin_get_by_name(GST_BIN(pipeline), "rate");
